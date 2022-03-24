1e2392526b9f03c0327eb44e2f58c529d284cdf0
RavenFajar <102020195+RavenFajar@users.noreply.github.com>
2022-03-24 21:41:15 +0700

    Update tugas.java

--- tugas.java
+++ tugas.java
@@ -5,6 +5,7 @@ public class tugas {
     public static void main(String[] args) {
         System.out.println("Tugas Modul 4");
         System.out.println("Aplikasi Ojek Online");
+        
 
     }
 }
