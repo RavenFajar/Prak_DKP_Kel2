b141ab76cbeea93137041ecdac22697286720907
RavenFajar <102020195+RavenFajar@users.noreply.github.com>
2022-03-24 21:37:42 +0700

    Add files via upload

---
+++ tugas.java
@@ -0,0 +1,10 @@
+package Gojek;
+import java.util.Scanner;
+
+public class tugas {
+    public static void main(String[] args) {
+        System.out.println("Tugas Modul 4");
+        System.out.println("Aplikasi Ojek Online");
+
+    }
+}
